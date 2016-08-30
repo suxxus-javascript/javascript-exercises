a5a10c714e9f178f07fbd677864948291749a690
suxxus <suxxus@outlook.com>
2016-08-30 23:18:46 +0200

    all tests

--- src/tests/index.js
+++ src/tests/index.js
@@ -1,8 +1,7 @@
 'use strict';
-/*
+
 require('./scripts/combine');
 require('./scripts/valid-parenthesess');
 require('./scripts/zeros-to-right');
 require('./scripts/divisible-mustache');
-*/
 require('./scripts/reverse-rotate');
