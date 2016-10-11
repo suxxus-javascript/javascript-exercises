4e245b0d3227a1368dbc9b895b4ed6e0f65ae0ff
suxxus <suxxus@outlook.com>
2016-10-11 08:41:00 +0200

    identation

--- src/tests/index.js
+++ src/tests/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-   require('./scripts/combine'); 
-   require('./scripts/valid-parenthesess');
-   require('./scripts/zeros-to-right');
-   require('./scripts/divisible-mustache');
-   require('./scripts/reverse-rotate');
-   require('./scripts/concatenating-functions');
-   require('./scripts/spacify');
+require('./scripts/combine');
+require('./scripts/valid-parenthesess');
+require('./scripts/zeros-to-right');
+require('./scripts/divisible-mustache');
+require('./scripts/reverse-rotate');
+require('./scripts/concatenating-functions');
+require('./scripts/spacify');
