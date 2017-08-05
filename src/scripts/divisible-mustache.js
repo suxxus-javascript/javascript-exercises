4de3ee696a9eb8c7e7fa1a9273cc25aa295365a8
suxxus <suxxus@outlook.com>
2017-08-05 04:58:31 +0200

    Updated divisible-mustache, to be more compact

--- src/scripts/divisible-mustache.js
+++ src/scripts/divisible-mustache.js
@@ -1,16 +1,19 @@
+// Complete the following conditions in a anonymous function below
+// The assertions on the right should all pass (they should all be green)
+//
+// The answers should be as follows:
+// If the number is divisible by three, you should return "beard"
+// If the number is divisible by five, you should return "mustache"
+// If the number is divisible by tree and five, you should return "beard mustache"
+// Otherwise you should return the number as a string.
+
 'use strict';
-module.exports = function(number) {
-
-  var w = {
-      '3': 'beard',
-      '5': 'mustache',
-      '7': 'eyebrows'
-    },
-
-    divisibles = Object.keys(w)
-    .reduce(function(acc, value) {
-      return ((number % value) === 0) ? acc.concat(w[value]) : acc;
-    }, []);
-
-  return divisibles.length === 0 ? String(number) : divisibles.join(' ');
+module.exports = function(words, number) {
+  return (function(value) {
+    return value.length === 0 ? String(number) : value.join(' ');
+  }(
+    Object.keys(words).reduce(function(acc, value) {
+      return ((number % value) === 0) ? acc.concat(words[value]) : acc;
+    }, [])
+  ));
 };
--- src/tests/scripts/divisible-mustache.js
+++ src/tests/scripts/divisible-mustache.js
@@ -1,4 +1,3 @@
-
 // Complete the following conditions in a anonymous function below
 // The assertions on the right should all pass (they should all be green)
 //
@@ -10,51 +9,58 @@
 
 'use strict';
 
+
+var words = {
+  '3': 'beard',
+  '5': 'mustache',
+  '7': 'eyebrows'
+};
+
 var test = require('tape');
 var divisibleMustache = require('scripts/divisible-mustache');
 
 test('is not divisible should return string', function(t) {
-  var actual = divisibleMustache(4),
+  var actual = divisibleMustache(words, 4),
     expect = '4';
   t.equal(actual, expect);
   t.end();
 });
 
 test('is divisible by three, should return beard', function(t) {
-  var actual = divisibleMustache(3),
+  var actual = divisibleMustache(words, 3),
     expect = 'beard';
   t.equal(actual, expect);
   t.end();
 });
 
 test('is divisible by five, should return mustache', function(t) {
-  var actual = divisibleMustache(5),
+  var actual = divisibleMustache(words, 5),
     expect = 'mustache';
   t.equal(actual, expect);
   t.end();
 });
 
 test('is divisible by seven, should return eyebrows', function(t) {
-  var actual = divisibleMustache(7),
+  var actual = divisibleMustache(words, 7),
     expect = 'eyebrows';
   t.equal(actual, expect);
   t.end();
 });
 
 test('is divisible by three and five, should return beard mustache', function(t) {
-  var actual = divisibleMustache(15),
+  var actual = divisibleMustache(words, 15),
     expect = 'beard mustache';
   t.equal(actual, expect);
   t.end();
 });
 
 test('is divisible by three and seven, should return beard eyebrows', function(t) {
-  var actual = divisibleMustache(21),
+  var actual = divisibleMustache(words, 21),
     expect = 'beard eyebrows';
   t.equal(actual, expect);
   t.end();
 });
 
 test('---- end test ----', function(t) {
-    t.end();
+  t.end();
 });
